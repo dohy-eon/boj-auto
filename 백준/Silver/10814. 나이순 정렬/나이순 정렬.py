31dff82e4f093f76fbc76d64054ba8315baf6310
Dohyeon Choi <do.hy824@gmail.com>
2026-04-13 09:21:10 +0900

    [Silver V] Title: 나이순 정렬, Time: 2652 ms, Memory: 46020 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+users = []
+for _ in range(n):
+    age, name = input().split()
+    users.append((int(age), name))
+
+users.sort(key=lambda x: x[0])
+
+for age, name in users:
+    print(age, name)
